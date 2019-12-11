75029a6857d03ef45b54a5a58c729cec71ce3a25
mia <genttunn@gmail.com>
2019-12-11 09:54:47 +0100

    beans added

--- src/main/java/ch/hevs/libraryService/library.java
+++ src/main/java/ch/hevs/libraryService/library.java
@@ -1,5 +1,21 @@
 package ch.hevs.libraryService;
 
-public interface library {
+import java.util.List;
 
+import ch.hevs.businessobject.Book;
+import ch.hevs.businessobject.Borrowing;
+import ch.hevs.businessobject.Customer;
+import ch.hevs.businessobject.Writer;
+
+public interface library
+{
+	
+	public List<Book> getBookListByWriterLastname(String lastname);
+	public List<Borrowing> getBorrowingListByCustomerLastname(String lastname);
+	public List<Book> getAllBooks();
+	public List<Customer>getCustomers();
+	public List<Writer>getWritters();
+	public String borrow(Customer borrower, Book borrowedBook) throws Exception;
+
+	
 }
--- src/main/java/ch/hevs/libraryService/libraryBean.java
+++ src/main/java/ch/hevs/libraryService/libraryBean.java
@@ -1,5 +1,48 @@
 package ch.hevs.libraryService;
 
+import java.util.List;
+
+import ch.hevs.businessobject.Book;
+import ch.hevs.businessobject.Borrowing;
+import ch.hevs.businessobject.Customer;
+import ch.hevs.businessobject.Writer;
+
 public class libraryBean implements library{
+
+	@Override
+	public List<Book> getBookListByWriterLastname(String lastname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Borrowing> getBorrowingListByCustomerLastname(String lastname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Writer> getWritters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String borrow(Customer borrower, Book borrowedBook) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
